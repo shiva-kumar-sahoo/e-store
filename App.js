b5837fb811fb3b40af826c0fcdc3d2f7f1d7d18d
Shiva Kumar Sahoo <codewithshivakumar@gmail.com>
2024-09-21 21:42:06 +0530

    add Statusbar component

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import "react-native-gesture-handler";
 import Navigation from "./Navigation";
+import { StatusBar } from "react-native";
 
 function App() {
-  return <Navigation />;
+  return (
+    <>
+      <Navigation />
+      <StatusBar style="auto" />
+    </>
+  );
 }
 
 export default App;
